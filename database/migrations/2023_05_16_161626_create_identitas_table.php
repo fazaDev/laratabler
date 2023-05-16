89bd72a48c3c0127613e456d2a61a8d4a2b7686f
Endi Iswanto <endy.lasax@gmail.com>
2023-05-16 23:23:52 +0700

    migration, model dan controller identitas

---
+++ database/migrations/2023_05_16_161626_create_identitas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('identitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_aplikasi', 100);
+            $table->text('deskripsi')->nullable();
+            $table->string('favicon', 100)->nullable();
+            $table->string('logo', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('identitas');
+    }
+};
